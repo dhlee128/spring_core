fe67f85849e5b7e34729699fc0c184926b8ea8a8
dahee <daheewa128@gmail.com>
2021-06-18 01:35:09 +0900

    자동 빈 등록

--- src/main/java/hello/spring_core/AutoAppConfig.java
+++ src/main/java/hello/spring_core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.spring_core;
 
+import hello.spring_core.member.MemberRepository;
+import hello.spring_core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,5 +11,8 @@ import static org.springframework.context.annotation.ComponentScan.*;
 @Configuration
 @ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class), basePackages = "hello.spring_core")
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
